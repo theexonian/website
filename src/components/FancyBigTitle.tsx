8157d09b2038e7198476ca554d319d41b2f7ddef
dodoleonarnci <davidozhang0316@gmail.com>
2023-12-24 15:52:52 +0800

    Feat: reponsiveness for laptop/mobile

--- src/components/FancyBigTitle.tsx
+++ src/components/FancyBigTitle.tsx
@@ -8,16 +8,15 @@ export default function FancyBigTitle({
 	if (!lowerText) {
 		return (
 			<div className="font-serif py-5">
-				<h1 className="text-5xl font-semibold -my-2">{title}</h1>
+			<h1 className="text-6xl font-semibold -my-2">{title}</h1>
 			</div>
 		);
 	} else {
 		return (
 			<div className="font-serif py-5">
-				<h1 className="text-5xl font-semibold -my-2">{title}</h1>
+				<h1 className="text-6xl font-semibold -my-2">{title}</h1>
 				<div className="py-1"></div>
 				<h1><i>{lowerText}</i></h1>
-				<hr className="w-1/3 border-black" />
 			</div>
 		);
 	}
