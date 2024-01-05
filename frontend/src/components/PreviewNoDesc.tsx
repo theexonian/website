d98816886aea7a2eab56bcfa1813da0f17edf6ab
Eric Li <ericlmtn@gmail.com>
2024-01-05 10:56:19 -0500

    Added server action to fetch most recent article in PreviewNoDesc

--- frontend/src/components/PreviewNoDesc.tsx
+++ frontend/src/components/PreviewNoDesc.tsx
@@ -1,24 +1,26 @@
-import Image from "next/image";
+import { getRecentArticle } from '@/actions/getRecentArticle';
 
-export default function PreviewNoDesc() {
+export default async function PreviewNoDesc() {
+	const article = await getRecentArticle();
+
 	return (
 		<div className="w-full p-3 border-neutral-300 border-b">
 			<div className="flex justify-between">
 				<div className="">
-					<h3 className="font-bold bg-gradient-to-r from-[#B40A0A] to-[#f71e1e] inline-block text-transparent bg-clip-text">Life</h3>
+					<h3 className="font-bold bg-gradient-to-r from-[#B40A0A] to-[#f71e1e] inline-block text-transparent bg-clip-text">
+						Life
+					</h3>
 					<h1 className="font-serif font-medium text-xl py-2">
-						Ah, I See That You Are Reading This
+						{article.attributes.title}
 					</h1>
 				</div>
 			</div>
-            <div className="">
-                <p className="text-xs text-[#6C6C6C]">
-                    By: Byran Huang
-                </p>
-                <p className="text-xs text-[#6C6C6C]">
-                    10/12/23
-                </p>
-            </div>
+			<div className="">
+				<p className="text-xs text-[#6C6C6C]">By: Byran Huang</p>
+				<p className="text-xs text-[#6C6C6C]">
+					{new Date(article.attributes.publishedAt).toLocaleDateString()}
+				</p>
+			</div>
 		</div>
 	);
 }
