e12a38f8d9dafcd4136218c8ea9ad4017cbf26f5
Eric Li <ericlmtn@gmail.com>
2024-01-18 11:56:52 -0500

    Added unstaged changes

--- frontend/src/actions/getRecentArticle.ts
+++ frontend/src/actions/getRecentArticle.ts
@@ -1,7 +1,8 @@
-import { fetchCached } from "@/lib/fetchRequests";
+import { fetchCached } from '@/lib/fetchRequests';
+import { ArticlesResponse } from '../../types/APIResponse';
 
 export async function getRecentArticle() {
-	const body = await fetchCached('http://127.0.0.1:1337/api/articles', {
+	const body: ArticlesResponse = await fetchCached('http://127.0.0.1:1337/api/articles?populate=*', {
 		headers: {
 			Authorization:
 				'Bearer e9dfcb43ebd0d2063bd563822849fce5fb6baaae2d9f69e625d40a50092f09e74afa8d6b7afff13593cd51ba298f5f6570e3b2ed0ffe8fb3beccc8eb8e957486fa8f875963dd1e6d058c2c0a184c621578c1503349a126d3ef098e1c322ae50aa7cb70569fac6bcd6bdbab63183dc5636322f2c910d44fc81ebd1b4ceecc9cd9',
--- frontend/src/components/PreviewNoDesc.tsx
+++ frontend/src/components/PreviewNoDesc.tsx
@@ -3,24 +3,35 @@ import { getRecentArticle } from '@/actions/getRecentArticle';
 export default async function PreviewNoDesc() {
 	const article = await getRecentArticle();
 
+	const authors = article.attributes.authors.data;
+
 	return (
 		<div className="w-full p-3 border-neutral-300 border-b">
-			<div className="flex justify-between">
-				<div className="">
-					<h3 className="font-bold bg-gradient-to-r from-[#B40A0A] to-[#f71e1e] inline-block text-transparent bg-clip-text">
-						Life
-					</h3>
-					<h1 className="font-serif font-medium text-xl py-2">
-						{article.attributes.title}
-					</h1>
+			<a href={`/articles/${article.attributes.title.toLowerCase().split(' ').join('-')}`}>
+				<div className="flex justify-between">
+					<div className="">
+						<h3 className="font-bold bg-gradient-to-r from-[#B40A0A] to-[#f71e1e] inline-block text-transparent bg-clip-text">
+							Life
+						</h3>
+						<h1 className="font-serif font-medium text-xl py-2">
+							{article.attributes.title}
+						</h1>
+					</div>
 				</div>
-			</div>
-			<div className="">
-				<p className="text-xs text-[#6C6C6C]">By: Byran Huang</p>
-				<p className="text-xs text-[#6C6C6C]">
-					{new Date(article.attributes.publishedAt).toLocaleDateString()}
-				</p>
-			</div>
+				<div className="">
+					<p className="text-xs text-[#6C6C6C]">
+						By:{' '}
+						<span>
+							{authors.map((author, i) => {
+								return author.attributes.fullname;
+							})}
+						</span>
+					</p>
+					<p className="text-xs text-[#6C6C6C]">
+						{new Date(article.attributes.publishedAt).toLocaleDateString()}
+					</p>
+				</div>
+			</a>
 		</div>
 	);
 }
