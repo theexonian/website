0483185bc32b05450adc9ef8b200288356d8bca0
Eric Li <ericlmtn@gmail.com>
2024-04-16 18:01:40 -0400

    Patched auto-description to call only on updates of content

--- backend/src/api/article/content-types/article/lifecycles.ts
+++ backend/src/api/article/content-types/article/lifecycles.ts
@@ -1,10 +1,11 @@
 export default {
   beforeUpdate(event) {
-	// Update description to be the first 100 characters of the content if description is not provided
+    // Update description to be the first 100 characters of the content if description is not provided
+	// This activates on each update of article content
     const { data } = event.params;
 
-    if (!data.description) {
-      event.params.data.description = data.content.substring(0, 100);
+    if (!data.description && data.content) {
+      event.params.data.description = data.content.substring(0, 100) + '...'
     }
   },
 };
