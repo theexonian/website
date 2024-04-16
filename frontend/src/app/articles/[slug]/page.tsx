c95bbd3379214d3083011c3aedae23eb147ed32b
Eric Li <ericlmtn@gmail.com>
2024-04-16 19:45:51 -0400

    Update article page to match latest style

--- frontend/src/app/articles/[slug]/page.tsx
+++ frontend/src/app/articles/[slug]/page.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+import { IoShareSocialOutline } from 'react-icons/io5';
+import { BiFontFamily, BiPrinter } from 'react-icons/bi';
+import { HiOutlineNewspaper } from 'react-icons/hi';
 import { getArticleById } from '@/actions/getArticleById';
 import Markdown from 'react-markdown';
 
@@ -8,8 +12,8 @@ export default async function Page({ params }: { params: { slug: string } }) {
 	if (!article) return null;
 
 	return (
-		<div className="flex h-auto items-center justify-center">
-			<article className="prose pt-8 font-serif prose-figcaption:font-sans prose-p:indent-8">
+		<div className="w-full flex justify-center">
+			<article className="prose md:prose-sm pt-8 font-serif prose-figcaption:font-sans prose-p:indent-8">
 				<h3 className="font-bold text-3xl bg-gradient-to-r from-[#B40A0A] to-[#f71e1e] inline-block text-transparent bg-clip-text m-0">
 					{article.tag.charAt(0).toUpperCase() + article.tag.slice(1)}
 				</h3>
@@ -32,7 +36,29 @@ export default async function Page({ params }: { params: { slug: string } }) {
 				<span className="p-0 m-0">
 					{new Date(article.publishedAt).toUTCString().split(' ').slice(0, 4).join(' ')}
 				</span>
-
+				<div className="flex flex-row gap-5">
+					<div className="flex items-center gap-2 text-red-700">
+						<IoShareSocialOutline className="text-xl" /> Share
+					</div>
+					<div className="flex items-center gap-2 text-red-700">
+						<BiFontFamily className="text-xl" /> Font
+					</div>
+					<div className="flex items-center gap-2 text-red-700">
+						<HiOutlineNewspaper className="text-xl" /> Publication
+					</div>
+					<div className="flex items-center gap-2 text-red-700">
+						<BiPrinter className="text-xl" /> Print
+					</div>
+				</div>
+				<hr className="border-neutral-400" />
+				<Image
+					src={'http://127.0.0.1:1337' + article.thumbnail.url}
+					width="0"
+					height="0"
+					sizes="25vw"
+					className="w-full h-auto"
+					alt={'Logo of The Exonian'}
+				/>
 				<Markdown>{article.content}</Markdown>
 			</article>
 		</div>
