35bc5df460108ad0936a8a1cad22e883ef2f47bf
Eric Li <ericlmtn@gmail.com>
2024-10-25 13:02:32 -0400

    Prepare website for AWS RDS

--- frontend/src/actions/getArticleById.ts
+++ frontend/src/actions/getArticleById.ts
@@ -16,8 +16,7 @@ export async function getArticleById(slug: string) {
 		`http://127.0.0.1:1337/api/articles?${query}`,
 		{
 			headers: {
-				Authorization:
-					'Bearer e9dfcb43ebd0d2063bd563822849fce5fb6baaae2d9f69e625d40a50092f09e74afa8d6b7afff13593cd51ba298f5f6570e3b2ed0ffe8fb3beccc8eb8e957486fa8f875963dd1e6d058c2c0a184c621578c1503349a126d3ef098e1c322ae50aa7cb70569fac6bcd6bdbab63183dc5636322f2c910d44fc81ebd1b4ceecc9cd9',
+				Authorization: `Bearer ${process.env.STRAPI_API}`,
 			},
 		}
 	);
--- frontend/src/actions/getArticleByZIndex.ts
+++ frontend/src/actions/getArticleByZIndex.ts
@@ -9,7 +9,7 @@ export async function getArticleByZIndex(z: number) {
 				$eq: z,
 			},
 		},
-		sort: ["createdAt:desc"],
+		sort: ['createdAt:desc'],
 		populate: '*',
 	});
 
@@ -17,8 +17,7 @@ export async function getArticleByZIndex(z: number) {
 		`http://127.0.0.1:1337/api/articles?${query}`,
 		{
 			headers: {
-				Authorization:
-					'Bearer e9dfcb43ebd0d2063bd563822849fce5fb6baaae2d9f69e625d40a50092f09e74afa8d6b7afff13593cd51ba298f5f6570e3b2ed0ffe8fb3beccc8eb8e957486fa8f875963dd1e6d058c2c0a184c621578c1503349a126d3ef098e1c322ae50aa7cb70569fac6bcd6bdbab63183dc5636322f2c910d44fc81ebd1b4ceecc9cd9',
+				Authorization: `Bearer ${process.env.STRAPI_API}`,
 			},
 		}
 	);
