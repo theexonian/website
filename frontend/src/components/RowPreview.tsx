426d98cd6628bc2427ef5e5cf0c2fb3ccd259c94
Byran Huang <byran8855@gmail.com>
2024-11-28 17:21:29 -0500

    fixed row previews and paragraph processing

--- frontend/src/components/RowPreview.tsx
+++ frontend/src/components/RowPreview.tsx
@@ -1,23 +1,31 @@
-interface ArticleRowPreview {
-	tag: string;
-	title: string;
-	authors: { slug: string; fullname: string }[];
-	description: string;
-	thumbnail: { url: string };
-	publishedAt: string;
+import Image from 'next/image';
+import { Author } from '../../types/APIResponse';
+
+interface ArticleRowPreviewProps {
+	article: {
+		title: string;
+		tag?: string;
+		description?: string;
+		publishedAt?: string;
+		authors?: Author[];
+		slug: string;
+		thumbnail: {
+			url: string;
+		};
+	};
 }
 
-export default function RowPreview({ article }: { article: ArticleRowPreview }) {
-	const { tag, title, authors, description, publishedAt } = article;
+export default function RowPreview(props: ArticleRowPreviewProps) {
+	const { title, tag, thumbnail, description, publishedAt, authors, slug } = props.article;
 
 	return (
 		// @TODO: refactor for props
 		<div className="w-full flex items-center py-3 border-neutral-300 border-b gap-4">
 			<div className="flex flex-col flex-wrap">
-				<a href="">
+				<a href={`/articles/${slug}`}>
 					<div className="w-full pr-3">
 						<h3 className="font-bold font-sans bg-gradient-to-r from-[#B40A0A] to-[#f71e1e] inline-block text-transparent bg-clip-text">
-							{tag.charAt(0).toUpperCase() + tag.slice(1)}
+							{tag ? tag.charAt(0).toUpperCase() + tag.slice(1) : ''}
 						</h3>
 						<h1 className="font-serif font-medium text-xl hover:text-neutral-600 duration-200">
 							{title}
@@ -32,7 +40,7 @@ export default function RowPreview({ article }: { article: ArticleRowPreview }) 
 				<div className="font-sans">
 					<p className="text-xs text-[#6C6C6C] duration-200">
 						By:&nbsp;
-						{authors.map((author, i) => {
+						{authors && authors.map((author, i) => {
 							return (
 								<a
 									className="text-xs hover:text-red-500 duration-200 no-underline"
