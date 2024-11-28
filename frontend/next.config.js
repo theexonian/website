721230215937c9067456b71532b0c033153f95fd
Byran Huang <byran8855@gmail.com>
2024-11-28 16:42:36 -0500

    reverse proxy the portal
    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,13 +3,21 @@ const nextConfig = {
 	images: {
 		remotePatterns: [
 			{
-				protocol: 'http',
-				hostname: '34.227.161.14',
-				port: '1337',
-				pathname: '/uploads/**',
+				protocol: "http",
+				hostname: "34.227.161.14",
+				port: "1337",
+				pathname: "/uploads/**",
 			},
 		],
 	},
+	async rewrites() {
+		return [
+			{
+				source: "/admin",
+				destination: "http://34.227.161.14:1337/",
+			},
+		];
+	},
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
