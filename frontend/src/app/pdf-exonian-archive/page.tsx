6ecd51d908cfcddf79ff31c854844142bb694f9c
Gavin-PM <gavin_yczhao@hotmail.com>
2025-04-10 21:22:24 -0400

    3rd try

--- frontend/src/app/pdf-exonian-archive/page.tsx
+++ frontend/src/app/pdf-exonian-archive/page.tsx
@@ -34,8 +34,8 @@ export default function Page() {
 	  }, []);
 
 	  const [windowSize, setWindowSize] = useState({
-		width: undefined,
-		height: undefined,
+		width: 0,
+		height: 0,
 	  });
 	
 	  useEffect(() => {
