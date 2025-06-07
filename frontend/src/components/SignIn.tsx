617d7651ec437f41970ce5feb940bb03b89cfe3a
Aaditya <abilak@gmail.com>
2025-06-07 14:17:47 -0400

    changed signin.tsx to always show and also used redirecttosignin (if this works, i'll change to modal)

--- frontend/src/components/SignIn.tsx
+++ frontend/src/components/SignIn.tsx
@@ -1,20 +1,12 @@
 'use client';
 
 import { useUser, useClerk } from '@clerk/nextjs';
-import { useEffect, useState } from 'react';
 
 export default function SignInButton() {
   const { user, isLoaded } = useUser();
   const clerk = useClerk();
-  const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted || !isLoaded) return null;
-
-  if (user) {
+  if (isLoaded && user) {
     return (
       <button onClick={() => clerk.signOut()} className="hover:text-red-400">
         Sign out
