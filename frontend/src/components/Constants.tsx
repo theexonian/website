ca6ef0e37814eb20619950bfb68e509872f72abc
Max Liu <max.liuyuan@hotmail.com>
2025-06-07 22:57:02 -0400

    Feat: Update Masthead and EIC

--- frontend/src/components/Constants.tsx
+++ frontend/src/components/Constants.tsx
@@ -1,15 +1,15 @@
 // Format: First Name (space) Last Name
 // Example: Roxane Park
-export const EIC_NAME = "Roxane Park"
+export const EIC_NAME = "Jinmin Lee"
 // Format: (apostrophe) Year of Graduation
 // Example: '25
-export const EIC_GRAD_YEAR = "'25"
+export const EIC_GRAD_YEAR = "'26"
 // Format: Email
 // Example: rhpark@exeter.edu
-export const EIC_EMAIL = "rhpark@exeter.edu"
+export const EIC_EMAIL = "jlee22@exeter.edu"
 // Format: Board Number (and suffix)
 // Example: 146th
-export const BOARD_NUMBER = "146th"
+export const BOARD_NUMBER = "147th"
 // Format: Public-facing AWS IPv4 address
 // Example: 34.227.161.14
 export const STRAPI_IP = "server.theexonian.net"
