f90a25d9451fd62305cd38c919c28eb9af8e765e
Gavin Zhao <gavin_yczhao@hotmail.com>
2026-03-01 16:15:25 -0500

    Change <img> to <Image> in auth pages

--- frontend/src/app/sign-in/[[...sign-in]]/page.tsx
+++ frontend/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,6 +3,7 @@
 import { SignIn } from '@clerk/nextjs';
 import CustomEmailSignIn from '@/components/CustomEmailSignIn';
 import { useEffect } from 'react';
+import Image from 'next/image';
 
 export default function Page() {
   // Redirect .com domains to .net for authentication
@@ -74,9 +75,11 @@ export default function Page() {
         {/* The Exonian Logo */}
         <div className="flex justify-center mt-8">
           <div className="text-center">
-            <img 
+            <Image 
               src="/logo.png" 
               alt="The Exonian Logo" 
+              width={320}
+              height={96}
               className="h-24 w-auto mx-auto mb-3 drop-shadow-lg"
             />
             <p className="text-xs text-gray-500 font-serif">
