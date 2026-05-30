035a30a12f99aab77f5bbe6653083eeedfa70adc
Gavin Zhao <gavin_yczhao@hotmail.com>
2026-05-30 15:30:55 -0400

    Init

---
+++ frontend/src/app/grad/page.tsx
@@ -0,0 +1,9 @@
+export default function GradPage() {
+  return (
+    <div className="px-4 sm:px-0 py-4 w-full">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-3xl font-bold font-serif">Grad</h1>
+      </div>
+    </div>
+  );
+}
