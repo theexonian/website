81d04c49fc1bb185b5246c39ea6d33a02428b1d3
Gavin Zhao <gavin_yczhao@hotmail.com>
2026-06-04 18:21:38 -0400

    Backend Strapi Fix

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,10 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env(
-    'PUBLIC_URL',
-    env('NODE_ENV') === 'production' ? 'https://server.theexonian.net' : 'http://localhost:1337'
-  ),
+  url: env('NODE_ENV') === 'production' ? 'https://server.theexonian.net' : 'http://localhost:1337',
   app: {
     keys: env.array('APP_KEYS'),
   },
